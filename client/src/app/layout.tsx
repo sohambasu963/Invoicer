aaf3d38aba1da8a663c9b96ac230b19fdf19bddc
Soham Basu <sohambasu963@gmail.com>
2023-12-27 09:42:42 -0500

    added firebase analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import firebase from './firebase';
 
 const inter = Inter({ subsets: ['latin'] });
 
